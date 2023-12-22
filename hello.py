6d1a87a031bdcde72e767b79103bd5463f818842
Lojsti <53957849+leotagliavini@users.noreply.github.com>
2023-12-22 18:25:10 -0300

    day 2

---
+++ hello.py
@@ -0,0 +1,15 @@
+# This is a comentary
+print("Hello python")
+"""This is comentary
+  with multiple 
+  lines
+  
+  xd
+
+
+"""
+# consultar tipo de dato
+print(type("What is this shit"))
+print(type(5))
+print(type(1 + 2j))
+print(type(1.5))
